6d15ebe083e311972362354ecd0d1ea0bbec6dc6
anusika <anusikanijher@gmail.com>
2023-02-14 17:31:14 -0800

    Add 370nm wide waveguide design

--- Layout/Designs/design_anusika.py
+++ Layout/Designs/design_anusika.py
@@ -36,16 +36,32 @@ def design_anusika(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
         'number_of_periods':30,
         'grating_period': 0.276,
         'corrugation_width': 0.05,
-        'wg_width': 0.350,
+        'wg_width': 0.370,
         'sinusoidal': True})
     if not cell_bragg:
         raise Exception ('Cannot load Bragg grating cell; please check the script carefully.')
+    
+
+    cell_taper = ly.create_cell('taper', library, {
+        'wg_width1': 0.35,
+        'wg_width2': 0.37,
+            })
+    if not cell_taper:
+        raise Exception ('Cannot load taper cell; please check the script carefully.')
+
+
+
+    waveguide_type_370 = 'Strip 1310 nm, w=370 nm (core-clad)'
 
     # instantiate y-branch (attached to input waveguide)
     inst_y1 = connect_cell(inst_wg1, 'opt2', cell_y, 'opt2')
 
-    # instantiate Bragg grating (attached to y branch)
-    inst_bragg1 = connect_cell(inst_y1, 'opt1', cell_bragg, 'opt1')
+
+    #ybranch to taper
+    inst_taper1 = connect_cell(inst_y1, 'opt1', cell_taper, 'pin1')
+
+    # instantiate Bragg grating (attached to taper)
+    inst_bragg1 = connect_cell(inst_taper1, 'pin2', cell_bragg, 'opt1')
 
     # instantiate Bragg grating (attached to the first Bragg grating)
     inst_bragg2 = connect_cell(inst_bragg1, 'opt2', cell_bragg, 'opt2')
@@ -56,7 +72,12 @@ def design_anusika(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     #####
     # Waveguides for the two outputs:
     connect_pins_with_waveguide(inst_y1, 'opt3', inst_wg3, 'opt1', waveguide_type=waveguide_type)
-    connect_pins_with_waveguide(inst_bragg2, 'opt1', inst_wg2, 'opt1', waveguide_type=waveguide_type)
+
+
+    #bragg to taper
+    inst_taper2 = connect_cell(inst_bragg2, 'opt1', cell_taper, 'pin2')
+
+    connect_pins_with_waveguide(inst_taper2, 'opt1', inst_wg2, 'opt1', waveguide_type=waveguide_type)
     
     '''
     make a long waveguide, back and forth,     
@@ -64,7 +85,7 @@ def design_anusika(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     using "turtle" routing
     https://github.com/SiEPIC/SiEPIC-Tools/wiki/Scripted-Layout#adding-a-waveguide-between-components
     '''
-    connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', waveguide_type=waveguide_type,
+    connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', waveguide_type=waveguide_type_370,
         turtle_A = [275,90,20,90,275,-90,20,-90,275,90,20,90,275,-90,20,-90, 275, 90, 20, 90, 275, -90, 20, -90] )
 
     return inst_wg1, inst_wg2, inst_wg3

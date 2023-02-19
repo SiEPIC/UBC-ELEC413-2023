8fe92a3840a9d34b0b932bf611f62b61a3abc42a
jonathan-hs-zhang <73516742+jonathan-hs-zhang@users.noreply.github.com>
2023-02-18 20:28:59 -0800

    Update design_jonathanz.py

--- Layout/Designs/design_jonathanz.py
+++ Layout/Designs/design_jonathanz.py
@@ -83,10 +83,10 @@ def design_jonathanz(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type)
     try:
         connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', 
             waveguide_type='Strip 1310 nm, w=370 nm (core-clad)',
-            turtle_A = [280,90,20,90,280,-90,20,-90,280,90,20,90,280,-90,20,-90, 280, 90, 20, 90, 280, -90, 20, -90] )
+            turtle_A = [250,90,20,90,250,-90,20,-90,250,90,20,90,250,-90,20,-90, 250, 90, 20, 90, 250, -90, 20, -90] )
     except:    
         connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', 
             waveguide_type='Strip 1310 nm, w=350 nm (core-clad)', 
-            turtle_A = [280,90,20,90,280,-90,20,-90,280,90,20,90,280,-90,20,-90, 280, 90, 20, 90, 280, -90, 20, -90] )
+            turtle_A = [250,90,20,90,250,-90,20,-90,250,90,20,90,250,-90,20,-90, 250, 90, 20, 90, 250, -90, 20, -90] )
 
     return inst_wg1, inst_wg2, inst_wg3

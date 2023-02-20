5f03d9e89bcf6a95bba800fcf9fdac39d0d15f11
hsw9822 <123869073+hsw9822@users.noreply.github.com>
2023-02-19 17:09:51 -0800

    Update design_Sangwon.py (#96)

--- Layout/Designs/design_Sangwon.py
+++ Layout/Designs/design_Sangwon.py
@@ -1,7 +1,14 @@
-from pya import *
 
 # Enter your Python code here
+from pya import *
+
+<<<<<<< HEAD:Layout/Designs/design_Sangwon.py
+# Enter your Python code here
 def design_Sangwon(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
+=======
+
+def design_Sangwon(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
+>>>>>>> parent of 1851630 (Final layout):Layout/Designs/design_Sangwonhwang.py
     
     # load functions
     from SiEPIC.scripts import connect_pins_with_waveguide, connect_cell
@@ -33,19 +40,35 @@ def design_Sangwon(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     # load the cells from the PDK
     # choose appropriate parameters
     cell_bragg = ly.create_cell('Bragg_grating', library, {
-        'number_of_periods':50 ,
+        'number_of_periods':40 ,
         'grating_period': 0.275,
         'corrugation_width': 0.035,
-        'wg_width': 0.35,
+        'wg_width': 0.370,
         'sinusoidal': False})
     if not cell_bragg:
         raise Exception ('Cannot load Bragg grating cell; please check the script carefully.')
+    
+
+    cell_taper = ly.create_cell('taper', library, {
+        'wg_width1': 0.35,
+        'wg_width2': 0.37,
+            })
+    if not cell_taper:
+        raise Exception ('Cannot load taper cell; please check the script carefully.')
+
+
+
+    waveguide_type_370 = 'Strip 1310 nm, w=370 nm (core-clad)'
 
     # instantiate y-branch (attached to input waveguide)
     inst_y1 = connect_cell(inst_wg1, 'opt2', cell_y, 'opt2')
 
-    # instantiate Bragg grating (attached to y branch)
-    inst_bragg1 = connect_cell(inst_y1, 'opt1', cell_bragg, 'opt1')
+
+    #ybranch to taper
+    inst_taper1 = connect_cell(inst_y1, 'opt1', cell_taper, 'pin1')
+
+    # instantiate Bragg grating (attached to taper)
+    inst_bragg1 = connect_cell(inst_taper1, 'pin2', cell_bragg, 'opt1')
 
     # instantiate Bragg grating (attached to the first Bragg grating)
     inst_bragg2 = connect_cell(inst_bragg1, 'opt2', cell_bragg, 'opt2')
@@ -56,19 +79,30 @@ def design_Sangwon(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     #####
     # Waveguides for the two outputs:
     connect_pins_with_waveguide(inst_y1, 'opt3', inst_wg3, 'opt1', waveguide_type=waveguide_type)
-    connect_pins_with_waveguide(inst_bragg2, 'opt1', inst_wg2, 'opt1', waveguide_type=waveguide_type)
-    
+
+
+    #bragg to taper
+    inst_taper2 = connect_cell(inst_bragg2, 'opt1', cell_taper, 'pin2')
+
+    connect_pins_with_waveguide(inst_taper2, 'opt1', inst_wg2, 'opt1', waveguide_type=waveguide_type)
     '''
     make a long waveguide, back and forth, 
     target 0.2 nm FSR assuming ng = 4
-    > wavelength=1270e-9; ng=4; fsr=0.2e-9;
+    > wavelength=1310e-9; ng=4; fsr=0.2e-9;
     > L = wavelength**2/2/ng/fsr
     > L * 1e6
-    > 1000 [microns]
+    > 1100 [microns]
     using "turtle" routing
     https://github.com/SiEPIC/SiEPIC-Tools/wiki/Scripted-Layout#adding-a-waveguide-between-components
     '''
-    connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', waveguide_type=waveguide_type,
-        #turtle_A = [250,90,20,90,250,-90,20,-90,250,90,20,90,250,-90,20,-90] )
-         turtle_A = [50])
+
+    try:
+        connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', 
+            waveguide_type='Strip 1310 nm, w=370 nm (core-clad)', 
+            turtle_A = [200,90,20,90,197,-90,20,-90,197,90,20,90,200,-90,20,-90] )
+    except:    
+        connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', 
+            waveguide_type='Strip 1310 nm, w=350 nm (core-clad)', 
+            turtle_A = [200,90,20,90,197,-90,20,-90,197,90,20,90,200,-90,20,-90] )
+
     return inst_wg1, inst_wg2, inst_wg3
